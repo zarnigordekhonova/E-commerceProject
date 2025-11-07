10811961a41b8cb3db8ddfdcb45060f4e0394007
Zarnigor <zarnigor1008@gmail.com>
2025-11-07 17:30:58 +0500

    First commit

---
+++ core/urls.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from django.urls import include, path
+
+from .schema import swagger_urlpatterns
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("", home, name="home"),
+    # path("api/common/", include("apps.common.urls", namespace="common")),
+    # path("api/accounts/", include("apps.accounts.urls", namespace="accounts")),
+    # path("api/blog/", include("apps.blog.urls", namespace="blog")),
+    # path("api/orders/", include("apps.orders.urls", namespace="orders")),
+    # path("api/products/", include("apps.products.paylov.urls", namespace="products")),
+]
+
+urlpatterns += swagger_urlpatterns
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
